91db6d860de841739345829127ac84babf0be27e
hakan.yedibela <hakan.yedibela@kps.com>
2023-02-04 17:53:16 +0100

    Initial Commit

---
+++ src/test/java/hy/dev/springbootplayground/SpringBootPlaygroundApplicationTests.java
@@ -0,0 +1,16 @@
+package hy.dev.springbootplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringBootPlaygroundApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
